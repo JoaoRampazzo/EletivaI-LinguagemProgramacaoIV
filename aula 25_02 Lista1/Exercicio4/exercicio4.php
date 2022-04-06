686c5d53a70b20d83736f44a731b8070640b40e1
JoaoRampazzo <99516140+JoaoRampazzo@users.noreply.github.com>
2022-04-06 20:10:22 -0300

    06_04

--- aula 25_02 Lista1/Exercicio4/exercicio4.php
+++ aula 25_02 Lista1/Exercicio4/exercicio4.php
@@ -6,28 +6,20 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercicio 2</title>
+    <title>Exercicio 4</title>
   </head>
   <body class="container mt-5 p-4 border border-gray border-2 rounded">
-    <h1>Exercicio 2</h1>
+    <h1>Exercico4</h1>
     
     <form method="post" action="resposta.php">
         <div class="row">
             <div class="col">
-              <label for="kilo" class="label-control">
-                Informe o valor do quilo do produto:
+              <label for="valor1" class="label-control">
+                Informe o valor:
               </label>
-              <input type="number" name="kilo"
-                id ="kilo" class="form-control"/>
+              <input type="number" name="valor"
+                id ="valor" class="form-control"/>
             </div>
-
-            <div class="col">
-            <label for="consumido" class="label-control">
-                Informe a quantidade consumida:
-              </label>
-              <input type="number" name="consumido"
-                  id="consumido" class="form-control"/>
-        </div>
 
         <div class="row mt-5">
           <div class="col">
