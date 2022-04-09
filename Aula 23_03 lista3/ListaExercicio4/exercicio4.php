b428f0b4ef0c31f9371c698eda3f7253f7b24719
JoaoRampazzo <99516140+JoaoRampazzo@users.noreply.github.com>
2022-04-08 21:33:15 -0300

    aaa

--- Aula 23_03 lista3/ListaExercicio4/exercicio4.php
+++ Aula 23_03 lista3/ListaExercicio4/exercicio4.php
@@ -8,34 +8,29 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Resposta exercício 3</title>
+    <title>Exercício 4</title>
   </head>
-  <body class="container">
-    <h1>Resposta exercício 3</h1>
-    
-    <?php
-    for ($i=1; $i<=5; $i++){
-        $vetor[$i] = $_POST["valor$i"]; 
-    }
-    
-    for ($i=1; $i<=5; $i++){
-      $valor = $vetor[$i];
-      $repetido = 0;
-
-      for ($j=1; $j<=5; $j++){
-        if ($valor == $vetor[$j]){
-          $repetido++;
-      }
-
-      }
-      if ($repetido > 1){
-        $vetor[$i] = '-';
-      }
-  }
-  var_dump($vetor);
-
-    ?>
-
+  <body class ="container">
+    <h1>Exercício 4</h1>
+    <form action="resposta.php" method="post">
+        <div class="row">
+            <?php
+            for ($i=1; $i<=5; $i++){
+            ?>
+            <div class="col">
+                <label for="valor<?= $i ?>" class="label-control">
+                    Infrome o valor <?= $i ?>
+                </label>
+                <input type="number" name="valor<?= $i ?>" id="valor<?= $i ?>"
+                    class="form-control"/>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-primary">Enviar</button>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- Aula 23_03 lista3/ListaExercicio4/resposta.php
+++ Aula 23_03 lista3/ListaExercicio4/resposta.php
@@ -8,31 +8,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Resposta exercício 3</title>
+    <title>Resposta exercício 4</title>
   </head>
   <body class="container">
-    <h1>Resposta exercício 3</h1>
+    <h1>Resposta exercício 4</h1>
     
     <?php
-    for ($i=1; $i<=5; $i++){
+    for ($i=1; $i<=10; $i++){
         $vetor[$i] = $_POST["valor$i"]; 
     }
     
-    for ($i=1; $i<=5; $i++){
-      $valor = $vetor[$i];
-      $repetido = 0;
-
-      for ($j=1; $j<=5; $j++){
-        if ($valor == $vetor[$j]){
-          $repetido++;
-      }
-
-      }
-      if ($repetido > 1){
-        $vetor[$i] = '-';
-      }
-  }
-  var_dump($vetor);
+    if ($valor = $valor);{
+        echo $valor = "-";
+    }
+    
 
     ?>
 
