e12a024afecd3604e2f3715137b9a29cecc83eaf
JoaoRampazzo <99516140+JoaoRampazzo@users.noreply.github.com>
2022-05-27 22:16:41 -0300

    27-05

--- ProjetoPHP_new/src/Controller/ClientesController.php
+++ ProjetoPHP_new/src/Controller/ClientesController.php
@@ -2,6 +2,7 @@
 
 namespace Aluno\ProjetoPhp\Controller;
 
+use Aluno\ProjetoPhp\Controller\ClientesController as ControllerClientesController;
 use Aluno\ProjetoPhp\Model\DAO\ClientesDAO;
 use Aluno\ProjetoPhp\Model\Entity\Clientes;
 
@@ -11,7 +12,15 @@ class ClientesController{
         require_once "../src/View/inserir_cliente.php";
     }
 
+    public static function abrirFormularioAlterar($params){
+        $dao = new ClientesDAO();
+        $resultado = $dao->consultarPorId($params[1]);
+        require_once "../src/View/alterar_cliente.php";
+    }
+
     public static function abrirListaClientes(){
+        $dao = new ClientesDAO();
+        $resultado = $dao->consultar();
         require_once "../src/View/listar_clientes.php";
     }
 
@@ -26,7 +35,34 @@ class ClientesController{
         } else {
             $resposta = false;
         }
-        require_once "../src/View/listar_clientes.php";
+
+        ControllerClientesController::abrirListaClientes();
+    }
+    public static function editarCliente($params){
+        $cliente = new Clientes();
+        $cliente->setEmail($_POST['email']);
+        $cliente->setIdade($_POST['idade']);
+        $cliente->setNome($_POST['nome']);
+        $cliente->setId($params[1]);
+        $dao = new ClientesDAO();
+        if ($dao->alterar($cliente)){
+            $resposta = true;
+        } else {
+            $resposta = false;
+        }
+
+        ControllerClientesController::abrirListaClientes();
+    }
+
+    public static function excluirCliente($params){
+        $dao = new ClientesDAO();
+        if ($dao->excluir($params[1])){
+            $resposta = true;
+        } else {
+            $resposta = false;
+        }
+
+        ControllerClientesController::abrirListaClientes();
     }
 
 }
